770d7391a1332a68970174c9a104e14fc8bc771c
anhhuy1806 <huyhero789@gmail.com>
2023-02-12 07:28:27 +0700

    images

--- src/app/fake-movies.ts
+++ src/app/fake-movies.ts
@@ -2,7 +2,7 @@ import { Movie } from "./model/movie";
 export const fakeMovies: Movie[] = [
     {
         id: 1,
-        imageName:'https://media.bongda.com.vn/files/hai.phan/2023/02/09/screen-shot-2023-02-09-at-093322-0934-1753.png',
+        imageName: "src/images/chelsea-01-1656.jpg",
         name: "Chuyển nhượng 10/02: Cập nhật vụ Fati, M.U hoàn tất HĐ mới; Chelsea đón thêm tân binh",
         conTent: "Barca sắp chia tay cầu thủ đầu tiên. Truyền thông Tây Ban Nha cho biết Barcelona sẽ bán 50% quyền sở hữu tiền vệ Francisco Trincao cho Sporting Lisbon trong mùa hè 2023. Mundo Deportivo tiết lộ Barca sẽ nhận 7 triệu euro từ CLB Bồ Đào Nha nếu thương vụ thành công. Đội chủ sân Camp Nou vẫn đánh giá cao tiềm năng của Trincao và có thể dùng 50% quyền sở hữu cầu thủ để mua lại anh trong tương lai. Khi đó, số tiền CLB xứ Catalonia phải chi sẽ vào khoảng 20-25 triệu euro. Tháng 8/2020, Barca bỏ ra 31 triệu euro để chiêu mộ Trincao từ Braga. Tuy nhiên, anh không tìm được chỗ đứng tại sân Camp Nou. "
     },
@@ -15,23 +15,23 @@ export const fakeMovies: Movie[] = [
     {
         id: 3,
         imageName:'https://media.bongda.com.vn/files/thach.pham/2023/02/09/1-1528.jpg',
-        name: "The Ghost and thedsadsaad Darkness",
-        conTent: "Barca sắp chia tay cầu thủ đầu tiên. Truyền thông Tây Ban Nha cho biết Barcelona sẽ bán 50% quyền sở hữu tiền vệ Francisco Trincao cho Sporting Lisbon trong mùa hè 2023. Mundo Deportivo tiết lộ Barca sẽ nhận 7 triệu euro từ CLB Bồ Đào Nha nếu thương vụ thành công. Đội chủ sân Camp Nou vẫn đánh giá cao tiềm năng của Trincao và có thể dùng 50% quyền sở hữu cầu thủ để mua lại anh trong tương lai. Khi đó, số tiền CLB xứ Catalonia phải chi sẽ vào khoảng 20-25 triệu euro. Tháng 8/2020, Barca bỏ ra 31 triệu euro để chiêu mộ Trincao từ Braga. Tuy nhiên, anh không tìm được chỗ đứng tại sân Camp Nou. "
+        name: "Glenn Hoddle muốn Declan Rice học hỏi Enzo Fernandez",
+        conTent: "Chỉ đến Chelsea chưa lâu, Enzo Fernandez đã mau chóng chứng tỏ được giá trị của mình. Anh mau chóng thích nghi với Premier League và đóng vai trò quan trọng trong lối chơi của The Blues. Trước West Ham, tuyển thủ Argentina có đường kiến tạo tuyệt vời cho Joao Felix ghi bàn mở tỷ số trong hiệp một. Thật tiếc khi Chelsea không thể bảo toàn được chiến thắng bởi bàn gỡ hòa sau đó của Emerson. "
     },
     {
         id: 4,
         imageName:'https://media.bongda.com.vn/files/thach.pham/2023/02/09/fred-1528.png',
-        name: "The Ghost and thedsadsaad Darkness",
+        name: "Thua tan tác 1-4, Tottenham lỡ cơ hội bám sát Man Utd                                ",
         conTent: "Barca sắp chia tay cầu thủ đầu tiên. Truyền thông Tây Ban Nha cho biết Barcelona sẽ bán 50% quyền sở hữu tiền vệ Francisco Trincao cho Sporting Lisbon trong mùa hè 2023. Mundo Deportivo tiết lộ Barca sẽ nhận 7 triệu euro từ CLB Bồ Đào Nha nếu thương vụ thành công. Đội chủ sân Camp Nou vẫn đánh giá cao tiềm năng của Trincao và có thể dùng 50% quyền sở hữu cầu thủ để mua lại anh trong tương lai. Khi đó, số tiền CLB xứ Catalonia phải chi sẽ vào khoảng 20-25 triệu euro. Tháng 8/2020, Barca bỏ ra 31 triệu euro để chiêu mộ Trincao từ Braga. Tuy nhiên, anh không tìm được chỗ đứng tại sân Camp Nou. "
     },
     {
-        id: 1,
+        id: 5,
         imageName:'https://media.bongda.com.vn/files/thach.pham/2023/02/09/fred-1528.png',
         name: "The Ghost and thedsadsaad Darkness",
         conTent: "Barca sắp chia tay cầu thủ đầu tiên. Truyền thông Tây Ban Nha cho biết Barcelona sẽ bán 50% quyền sở hữu tiền vệ Francisco Trincao cho Sporting Lisbon trong mùa hè 2023. Mundo Deportivo tiết lộ Barca sẽ nhận 7 triệu euro từ CLB Bồ Đào Nha nếu thương vụ thành công. Đội chủ sân Camp Nou vẫn đánh giá cao tiềm năng của Trincao và có thể dùng 50% quyền sở hữu cầu thủ để mua lại anh trong tương lai. Khi đó, số tiền CLB xứ Catalonia phải chi sẽ vào khoảng 20-25 triệu euro. Tháng 8/2020, Barca bỏ ra 31 triệu euro để chiêu mộ Trincao từ Braga. Tuy nhiên, anh không tìm được chỗ đứng tại sân Camp Nou. "
     },
     {
-        id: 1,
+        id: 6,
         imageName:'https://media.bongda.com.vn/files/hai.phan/2023/02/09/mg-off-platform-kane-man-u-copy-1753.jpg',
         name: "The Ghost and thedsadsaad Darkness",
         conTent: "Barca sắp chia tay cầu thủ đầu tiên. Truyền thông Tây Ban Nha cho biết Barcelona sẽ bán 50% quyền sở hữu tiền vệ Francisco Trincao cho Sporting Lisbon trong mùa hè 2023. Mundo Deportivo tiết lộ Barca sẽ nhận 7 triệu euro từ CLB Bồ Đào Nha nếu thương vụ thành công. Đội chủ sân Camp Nou vẫn đánh giá cao tiềm năng của Trincao và có thể dùng 50% quyền sở hữu cầu thủ để mua lại anh trong tương lai. Khi đó, số tiền CLB xứ Catalonia phải chi sẽ vào khoảng 20-25 triệu euro. Tháng 8/2020, Barca bỏ ra 31 triệu euro để chiêu mộ Trincao từ Braga. Tuy nhiên, anh không tìm được chỗ đứng tại sân Camp Nou. "
